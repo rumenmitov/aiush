c6570e0ec91a52b886ae38f4eab7223b7aa46f3b
Rumen <rumenmitov@protonmail.com>
2024-12-01 12:36:39 +0100

    update: separated unit tests, close #21

---
+++ tests/test-history.cpp
@@ -0,0 +1,22 @@
+#include "history.hpp"
+
+#include <unistd.h>
+#include <cassert>
+
+    
+int main(void) {
+    History::CmdHistory hist;
+
+    hist.update("echo");
+    hist.update("cd");
+
+    hist.update("echo");
+    hist.update("echo");
+
+    sleep(1);
+    hist.update("cd");
+
+    assert(hist.get_score("cd") < hist.get_score("echo"));
+    return 0;
+}
+
